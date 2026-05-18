7c85560fea303aaf0ba75d307caa9c180960bca6
arthurcomerlattoo <arthurcomerlatto@gmail.com>
2026-05-18 02:20:48 -0300

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-DXHUuDM6.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/user/Desktop/vscode/Projetos_capacitação_AutoJun/Task/taskflow-studio/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-BNimoohw.js"] }, "/": { filePath: "C:/Users/user/Desktop/vscode/Projetos_capacitação_AutoJun/Task/taskflow-studio/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-BnsTAzP7.js"] } }, clientEntry: "/assets/index-BNimoohw.js" });
+export {
+  tsrStartManifest
+};
---
+++ server/index.js
@@ -0,0 +1,5 @@
+import { w } from "./assets/worker-entry-CHJgynaT.js";
+import "node:events";
+export {
+  w as default
+};
