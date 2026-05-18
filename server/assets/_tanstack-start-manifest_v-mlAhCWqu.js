070f775416300fab9064fdbc323b618305485c3a
arthurcomerlattoo <arthurcomerlatto@gmail.com>
2026-05-17 23:21:10 -0300

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-mlAhCWqu.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/user/Desktop/vscode/Projetos_capacitação_AutoJun/Task/taskflow-studio/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/taskflow-studio/assets/index-CySCGo8g.js"] }, "/": { filePath: "C:/Users/user/Desktop/vscode/Projetos_capacitação_AutoJun/Task/taskflow-studio/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/taskflow-studio/assets/index-CIpkPT7w.js"] } }, clientEntry: "/taskflow-studio/assets/index-CySCGo8g.js" });
+export {
+  tsrStartManifest
+};
---
+++ server/index.js
@@ -0,0 +1,5 @@
+import { w } from "./assets/worker-entry-BpA5UHj5.js";
+import "node:events";
+export {
+  w as default
+};
